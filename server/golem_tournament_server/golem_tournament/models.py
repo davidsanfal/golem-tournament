fd36f2976c6ae73328a802c707d1eb994eb8d16d
David Sanchez Falero <david.sanchez.falero@gmail.com>
2016-11-08 10:45:09 +0100

    new golem model

--- server/golem_tournament_server/golem_tournament/models.py
+++ server/golem_tournament_server/golem_tournament/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-import jsonfield
 
 
 class Part(models.Model):
@@ -13,13 +12,13 @@ class Part(models.Model):
 
 class Weapon(Part):
     weapon_range = models.IntegerField(default=0)
-    damage = models.IntegerField(default=1)
+    damage = models.IntegerField(default=0)
     speed = models.IntegerField(default=1)
     limit = models.IntegerField(default=0)
 
 
 class Armor(Part):
-    defense = models.IntegerField(default=1)
+    defense = models.IntegerField(default=0)
 
 
 class Engine(Part):
@@ -27,16 +26,34 @@ class Engine(Part):
 
 
 class Golem(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='golems', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User',
+                              related_name='golems',
+                              on_delete=models.CASCADE)
     name = models.CharField(max_length=60)
     golem_type = models.CharField(max_length=60)
-    parts = jsonfield.JSONField(default = {'engine': None,
-                                           'weapons': {'r': None,
-                                                       'l': None},
-                                            'armors': {'legr': None,
-                                                       'legl': None,
-                                                       'armr': None,
-                                                       'arml': None,
-                                                       'cabin': None,
-                                                       'head': None},
-                                            })
+    engine = models.IntegerField(default=0)
+    right_weapon = models.IntegerField(default=0)
+    left_weapon = models.IntegerField(default=0)
+    right_leg = models.IntegerField(default=0)
+    left_leg = models.IntegerField(default=0)
+    right_arm = models.IntegerField(default=0)
+    left_arm = models.IntegerField(default=0)
+    cabin = models.IntegerField(default=0)
+    helm = models.IntegerField(default=0)
+
+    def can_be_equipped(self, part):
+        return part.golem_type == self.golem_type
+
+    def equip_item(self, part):
+        setattr(self, part.part_type, part.id)
+
+    def equipment(self):
+        return {'engine': Engine.objects.get(pk=self.engine),
+                'weapons': {'right': Weapon.objects.get(pk=self.right_weapon),
+                            'left': Weapon.objects.get(pk=self.right_weapon)},
+                'armors': {'right_leg': Armor.objects.get(pk=self.right_leg),
+                           'left_leg': Armor.objects.get(pk=self.left_leg),
+                           'right_arm': Armor.objects.get(pk=self.right_arm),
+                           'left_arm': Armor.objects.get(pk=self.left_arm),
+                           'cabin': Armor.objects.get(pk=self.cabin),
+                           'head': Armor.objects.get(pk=self.head)}}
