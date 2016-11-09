55d826d35ef7540929e81a58db3ebf8f7b17c2d1
David Sanchez Falero <david.sanchez.falero@gmail.com>
2016-11-09 16:11:03 +0100

    generic views

--- server/golem_tournament_server/golem_tournament/views.py
+++ server/golem_tournament_server/golem_tournament/views.py
@@ -11,86 +11,46 @@ from golem_tournament.permissions import IsOwnerOrReadOnly, IsAuthenticatedOrCre
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 
-class SignUp(APIView):
+class SignUp(generics.CreateAPIView):
     permission_classes = (IsAuthenticatedOrCreate,)
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GolemCreator(generics.CreateAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Golem.objects.all()
+    serializer_class = GolemSerializer
 
 
-class UserList(APIView):
+class UserList(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GolemList(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Golem.objects.all()
+    serializer_class = GolemSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
     permission_classes = (permissions.IsAuthenticated,)
 
-    def get(self, request, format=None):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-
-class UserDetail(APIView):
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get_object(self, username):
+    def get_by_name(self, username):
         try:
             return User.objects.get(username=username)
         except User.DoesNotExist:
             raise Http404
 
     def get(self, request, username):
-        print(username)
-        user = self.get_object(username)
+        user = self.get_by_name(username)
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
 
-class GolemList(APIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def get(self, request, format=None):
-        golems = Golem.objects.all()
-        serializer = GolemSerializer(golems, many=True)
-        return Response(serializer.data)
-
-
-class GolemDetail(APIView):
+class GolemDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
-
-    def get_object(self, pk):
-        try:
-            return Golem.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        golem = self.get_object(pk)
-        serializer = GolemSerializer(golem)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        golem = self.get_object(pk)
-        serializer = GolemSerializer(golem, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        golem = self.get_object(pk)
-        golem.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class GolemCreator(APIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def post(self, request):
-        print(request.user.id)
-        serializer = GolemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=self.request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Golem.objects.all()
+    serializer_class = GolemSerializer
